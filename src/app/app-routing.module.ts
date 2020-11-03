80f4614eac0ffe1c0bb9b471eb2bc471d83134d9
Duran <duran@MBP-Duran.local>
2020-11-03 00:01:32 -0600

    Added settings page structure, form-input styles and reactive forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SettingsComponent } from './pages/settings/settings.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: "", component: SettingsComponent},
+  { path: "settings", component: SettingsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
