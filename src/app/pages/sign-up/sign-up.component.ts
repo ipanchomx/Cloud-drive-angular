89f8ad82ab1af489bb2789f823f96ff1b06f77ea
unknown <is715773@iteso.mx>
2020-11-06 10:05:32 -0600

    Added sign up and login forms, added design

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -11,5 +12,10 @@ export class SignUpComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
 
 }
