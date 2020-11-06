2a2a49f9ad4f5bc84b2004ef55ed8b64fcd17505
unknown <is715773@iteso.mx>
2020-11-06 01:33:57 -0600

    Added sign  up page path to routing (typo correction)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { SettingsComponent } from './pages/settings/settings.component';
 import { PendingFilesSharedComponent } from './pages/pending-files-shared/pending-files-shared.component'
 import { FileManagerComponent } from './pages/file-manager/file-manager.component';
 import { HomeComponent } from './pages/home/home.component';
-import { SignUpComponent } from './pages/home/sign-up.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
