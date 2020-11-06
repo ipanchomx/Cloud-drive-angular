c84bf5d14b7753bd32b0650b0629d5ae1e9684dc
unknown <is715773@iteso.mx>
2020-11-05 21:57:56 -0600

    Angular Material  imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavBarComponent } from './globals/layouts/nav-bar/nav-bar.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
@@ -18,7 +24,12 @@ import { MatSliderModule } from '@angular/material/slider';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatSliderModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSlideToggleModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
