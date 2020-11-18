1a3386dbad084a91c299586febe1a55b8933df4a
Duran <duran@MBP-Duran.local>
2020-11-17 22:28:16 -0600

    Added modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SocialLoginModule, SocialAuthServiceConfig, GoogleLoginProvider } from 'angularx-social-login';
 
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
@@ -14,8 +16,10 @@ import { GridListComponent } from './components/grid-list/grid-list.component';
 import { PendingFilesSharedComponent } from './pages/pending-files-shared/pending-files-shared.component';
 import { FileInformationComponent } from './pages/file-information/file-information.component';
 import { FileManagerComponent } from './pages/file-manager/file-manager.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+// import { UploadFileFormComponent } from './dialogs/upload-file-form/upload-file-form.component';
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatMenuModule } from '@angular/material/menu';
@@ -28,10 +32,11 @@ import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatListModule } from '@angular/material/list';
-import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { HttpClientModule } from '@angular/common/http';
+import {MatGridListModule} from '@angular/material/grid-list';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
-import { SocialLoginModule, SocialAuthServiceConfig, GoogleLoginProvider } from 'angularx-social-login';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 
 import { environment } from './../environments/environment';
 @NgModule({
@@ -47,17 +52,20 @@ import { environment } from './../environments/environment';
     FileInformationComponent,
     GridListComponent,
     FileManagerComponent,
-    SignUpComponent
+    SignUpComponent,
+    UploadFileFormComponent
   ],
-  imports : [
+  imports: [
     AppRoutingModule,
     HttpClientModule,
+    SocialLoginModule,
     FormsModule,
     MatSliderModule,
     MatMenuModule,
     MatIconModule,
     MatSlideToggleModule,
     MatSidenavModule,
+    MatSelectModule,
     MatSelectModule,
     MatInputModule,
     MatCardModule,
@@ -68,7 +76,9 @@ import { environment } from './../environments/environment';
     BrowserAnimationsModule,
     MatButtonModule,
     MatSnackBarModule,
-    SocialLoginModule
+    MatGridListModule,
+    MatRadioModule,
+    MatAutocompleteModule
   ],
   providers: [
     {
