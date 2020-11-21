3d973c220fae13fa0d15fbec4ec04aaac470f579
Duran <duran@MBP-Duran.local>
2020-11-20 20:59:56 -0600

    Created Files Service with initial methods

---
+++ src/app/globals/services/files.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from "./../../../environments/environment"
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FilesService {
+  constructor(private httpClient:HttpClient, private _authService: AuthService) { }
+
+  upload(data: any){
+    const url = `${environment.apiUrl}files/`;
+    const httpHeaders = new HttpHeaders({
+      Authorization: this._authService.get()
+    });
+    return this.httpClient.post(url, data, {
+      headers: httpHeaders,
+      reportProgress: true,
+      observe: 'events'
+    });
+  }
+
+
+  getPathContent(path: string) {
+    const url = `${environment.apiUrl}files?path=${path}`;
+    const httpHeaders = new HttpHeaders({
+      Authorization: this._authService.get()
+    });
+    return this.httpClient.get(url, {
+      headers: httpHeaders,
+    }).toPromise();
+  }
+}
