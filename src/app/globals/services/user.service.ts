d7adb36dfde1f23383ee7d79e63e094bacffb481
Duran <duran@MBP-Duran.local>
2020-11-22 15:02:50 -0600

    Auth headers in middleware

--- src/app/globals/services/user.service.ts
+++ src/app/globals/services/user.service.ts
@@ -1,18 +1,25 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { environment } from "./../../../environments/environment"
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private httpClient:HttpClient) { }
+  constructor(private httpClient:HttpClient, private _authService: AuthService) { }
 
   getUsers(searchInput: string = '') {
+    const httpHeaders = new HttpHeaders({
+      Authorization: this._authService.get()
+    });
+
     const url = `${environment.apiUrl}users?q=${searchInput}`;
 
-    return this.httpClient.get(url);
+    return this.httpClient.get(url, {
+      headers: httpHeaders
+    });
   }
 }
