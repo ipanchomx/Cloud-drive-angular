f8ef738b481b0de8cfbf8eab8d49bc2311710e6f
Duran <duran@MBP-Duran.local>
2020-11-22 01:11:33 -0600

    Created File interface models

--- src/app/globals/models/file.model.ts
+++ src/app/globals/models/file.model.ts
@@ -1,4 +1,24 @@
 export interface filesResponse {
-    files: any[],
-    folders: any[]
+    files: File[],
+    folders: File[]
+}
+
+export interface FileComment {
+    date: string,
+    comment: string,
+    author: string
+}
+
+export interface File {
+    fileName: string,
+    dateOfCreation: string,
+    owner: string,
+    path: string,
+    status: string,
+    requiresVerification: boolean,
+    verificationStatus: string, 
+    storageId: string,
+    sharedWith: any[],
+    isDirectory: boolean,
+    comments: FileComment[]
 }
