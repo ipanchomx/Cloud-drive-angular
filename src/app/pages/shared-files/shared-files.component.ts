faa0b244b626e0a70e8b91a1221cc7e9c36cc9b5
unknown <is715773@iteso.mx>
2020-11-21 20:49:52 -0600

    Added shared files component

---
+++ src/app/pages/shared-files/shared-files.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shared-files',
+  templateUrl: './shared-files.component.html',
+  styleUrls: ['./shared-files.component.scss']
+})
+export class SharedFilesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
