f085ae12cddf89f6d92942083d1778073a5d1f7f
Edgar Medina <is715468@iteso.mx>
2020-11-22 21:14:48 -0600

    Added component interaction with services to get data

--- src/app/pages/shared-files/shared-files.component.ts
+++ src/app/pages/shared-files/shared-files.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { File, filesResponse } from 'src/app/globals/models/file.model';
+import { FilesService } from 'src/app/globals/services/files.service';
 
 @Component({
   selector: 'app-shared-files',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SharedFilesComponent implements OnInit {
 
-  constructor() { }
+  files: File[] = [];
+  folders: File[] = [];
+  inProgress: boolean = false;
+  path: string = '/';
+
+  constructor(private _filesService: FilesService) { }
 
   ngOnInit(): void {
+    this.getSharedContent();
   }
 
+  goToFolderPath($event) {
+    console.log($event);
+    if (this.path != '/') this.path += '/';
+    this.path += $event.fileName;
+    this.getSharedContent();
+  }
+
+  getSharedContent() {
+    this.inProgress = true;
+    this._filesService.getSharedContent(this.path)
+      .then((res: filesResponse) => {
+        if (res && res.files && res.folders) {
+          this.files = res.files;
+          this.files.sort((file1, file2) => (file2.fileName <= file1.fileName) ? 1 : -1);
+          this.folders = res.folders;
+          this.folders.sort((folder1, folder2) => (folder2.fileName <= folder1.fileName) ? 1 : -1);
+          console.log(res)
+          this.inProgress = false;
+
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        this.inProgress = false;
+      });
+  }
 }
