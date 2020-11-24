104a3faae931a5be91f576b47b76d53b6bb48086
unknown <is715773@iteso.mx>
2020-11-23 18:47:48 -0600

    Sends notifications from navbar

--- src/app/globals/layouts/nav-bar/nav-bar.component.ts
+++ src/app/globals/layouts/nav-bar/nav-bar.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import{NotificationsComponent} from 'src/app/dialogs/notifications/notifications.component';
+
+export interface notification {
+  message: string;
+  date: Date;
+  emiterEmail: string;
+  fileName: string;
+}
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,8 +18,43 @@ import { AuthService } from '../../services/auth.service';
 })
 export class NavBarComponent implements OnInit {
   isLoggedIn:boolean = false;
-
-  constructor(private authService: AuthService, private router: Router) { 
+  
+  name = "anai";
+  notifications: notification[] = [
+    {
+      message: 'shared a file with you',
+      date: new Date(),
+      emiterEmail: 'ejemplo@gmail.com',
+      fileName: 'presupuesto.xxl'
+    },
+    {
+      message: 'shared a file with you',
+      date: new Date(),
+      emiterEmail: 'ej2@gmail.com',
+      fileName: 'img.png'
+    },
+    {
+      message: 'updated a file',
+      date: new Date(),
+      emiterEmail: 'amoLosGatos@gmail.com',
+      fileName: 'ensayoGatos.docx'
+    },
+    {
+      message: 'shared a file with you',
+      date: new Date(),
+      emiterEmail: 'amoLosGatos@gmail.com',
+      fileName: 'ensayoGatos.docx'
+    },
+    {
+      message: 'commented on a file',
+      date: new Date('11/23/20'),
+      emiterEmail: 'panchito@gmail.com',
+      fileName: 'precios.xxl'
+    }
+  ];
+  noSize = this.notifications.length; 
+  
+  constructor(private authService: AuthService, private router: Router,private _matDialog: MatDialog) { 
 
     this.authService.loginStatus.subscribe(status=>{
       this.isLoggedIn = status;
@@ -24,6 +68,20 @@ export class NavBarComponent implements OnInit {
   logout() {
     this.authService.clear();
     this.router.navigate(['/home']);
+  }
+
+  openNotifications() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.minWidth = "20px";
+    dialogConfig.minHeight = "10px";
+    dialogConfig.position = { top: '50px', right: '50px' };
+    dialogConfig.data =  {notifications: this.notifications, name: this.name};
+
+    const dialogRef = this._matDialog.open(NotificationsComponent, dialogConfig);
+    dialogRef.afterClosed().subscribe(result => {
+      this.noSize = 0;
+    });
+
   }
 
 }
