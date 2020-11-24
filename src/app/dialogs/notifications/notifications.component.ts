591a751c0d700c0c205710e7a40395a863829f95
unknown <is715773@iteso.mx>
2020-11-23 18:47:16 -0600

    Added notifications functionality

--- src/app/dialogs/notifications/notifications.component.ts
+++ src/app/dialogs/notifications/notifications.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface notification {
+  message: string;
+  date: Date;
+  emiterEmail: string;
+  fileName: string;
+}
+
 
 @Component({
   selector: 'app-notifications',
@@ -6,10 +15,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notifications.component.scss']
 })
 export class NotificationsComponent implements OnInit {
+  
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private dialogRef: MatDialogRef<NotificationsComponent>) { }
 
-  constructor() { }
 
   ngOnInit(): void {
+    console.log(this.data)
+  }
+
+  onClose(): void {
+    this.dialogRef.close();
   }
 
 }
