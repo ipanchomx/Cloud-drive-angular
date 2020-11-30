57c1c1d4ce7f472e04035f1f31e0e05ed474bce9
mauduran <is716122@iteso.mx>
2020-11-30 13:50:45 -0600

    Fixed npm commands and environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "http://localhost:3000/api/",
-  socketUrl: 'http://localhost:3000',
+  apiUrl: "https://cloud-drive-app.herokuapp.com/api/",
+  socketUrl: 'https://cloud-drive-app.herokuapp.com/',
   clientId: "1096369333431-u7kmh0300srbn1cos4ikgo7f2soe92je.apps.googleusercontent.com"
 };
