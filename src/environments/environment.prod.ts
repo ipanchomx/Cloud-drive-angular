19ff1bef08f78367c37b2879402bf5cdebf71906
mauduran <is716122@iteso.mx>
2020-11-30 16:00:50 -0600

    Changed google client id

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: "https://cloud-drive-app.herokuapp.com/api/",
   socketUrl: 'https://cloud-drive-app.herokuapp.com/',
-  clientId: "1096369333431-u7kmh0300srbn1cos4ikgo7f2soe92je.apps.googleusercontent.com"
+  clientId: "1096369333431-rq49l7sq74v8dskr4hpmp6s5sup9dlip.apps.googleusercontent.com"
 };
