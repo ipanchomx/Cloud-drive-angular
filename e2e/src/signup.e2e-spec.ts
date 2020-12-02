fbabfe0d1032ec62e01e8118603cea3270b7cbf3
unknown <is715773@iteso.mx>
2020-12-02 00:46:26 -0600

    Added user already exists test

--- e2e/src/signup.e2e-spec.ts
+++ e2e/src/signup.e2e-spec.ts
@@ -42,6 +42,37 @@ describe('workspace-project App', () => {
     expect(successAlert.getText()).toEqual('User successfully created');
   });
 
+  it('should not sign up if email already exist', () => {
+    
+    browser.waitForAngularEnabled(false)
+    page.navigateTo();
+
+    const getStartedButton = element(by.id("get-started"))
+    getStartedButton.click();
+
+    browser.wait(() => element(by.className('register')).getText(), 3000);
+
+    const email = 't@t.com';
+    const name = 'test';
+    const password = '123456';
+
+    const emailInput = element(by.id('signupEmail'));
+    const nameInput = element(by.id('signupName'));
+    const passwordInput = element(by.id('signupPassword'));
+    const passwordCInput = element(by.id('passwordConfirm'));
+    const loginButton = element(by.id('signUpButton'));
+
+    emailInput.sendKeys(email);
+    nameInput.sendKeys(name);
+    passwordInput.sendKeys(password);
+    passwordCInput.sendKeys(password);
+
+    loginButton.click();
+    browser.sleep(1000);
+
+    const successAlert = element(by.css('.mat-simple-snackbar span'))
+    expect(successAlert.getText()).toEqual('User already exists!');
+  });
 
 
   afterEach(async () => {
